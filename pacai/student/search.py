9f38d7dd963f9b9e82f84ca3381a287537bae514
Nick <nkorien@ucsc.edu>
2022-04-10 15:10:30 -0700

    ufs working

--- pacai/student/search.py
+++ pacai/student/search.py
@@ -79,12 +79,39 @@ def breadthFirstSearch(problem):
 
 
 def uniformCostSearch(problem):
-    """
-    Search the node of least total cost first.
-    """
+    # node:
+    # [0] = state;
+    # [1] = actions from to start to state;
+    # [2] = cost
 
-    # *** Your Code Here ***
-    raise NotImplementedError()
+    if problem.isGoal(problem.startingState()):
+        return []
+
+    frontier = priorityQueue.PriorityQueue()
+    reached = {}
+
+    node = (problem.startingState(), [], 0)
+    frontier.push(node, 0)
+
+    while not frontier.isEmpty():
+        node = frontier.pop()
+
+        if node[0] in reached:
+            if not node[2] < reached[node[0]]:
+                continue
+
+        if problem.isGoal(node[0]):
+            return node[1]
+
+        reached[node[0]] = node[2]
+        expand = problem.successorStates(node[0])
+
+        for child in expand:
+            cost = node[2] + child[2]
+            childNode = (child[0], node[1] + [child[1]], cost)
+            frontier.push(childNode, cost)
+
+    return FAILURE
 
 
 def aStarSearch(problem, heuristic):
